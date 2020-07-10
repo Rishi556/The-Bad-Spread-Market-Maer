43d15af590af4e731ed7eb0c3076cc15f99fd7dd
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2020-07-10 14:07:53 +0100

    Fix placing orders that don't need to be placed, which waste RCs

--- src/cancel_current_orders.js
+++ src/cancel_current_orders.js
@@ -1,34 +1,34 @@
-let hive = require("@hiveio/hive-js")
-let config = require("../config.json")
-let place_orders = require("./place_orders.js")
-let axios = require("axios")
+let hive = require("@hiveio/hive-js");
+let config = require("../config.json");
+let place_orders = require("./place_orders.js");
+let axios = require("axios");
 
-let rpcAPI = "https://api.hive-engine.com/rpc/contracts"
+let rpcAPI = "https://api.hive-engine.com/rpc/contracts";
 
 function cancelOrders(){
-  let buyOrderQuery = {id: 0,jsonrpc: "2.0",method: "find",params: {contract: "market", table: "buyBook", query: {account : config.username, symbol : config.symbol}, limit: 1000, offset: 0, indexes: [{index: "_id", descending: true}]}}
+  let buyOrderQuery = {id: 0,jsonrpc: "2.0",method: "find",params: {contract: "market", table: "buyBook", query: {account : config.username, symbol : config.symbol}, limit: 1000, offset: 0, indexes: [{index: "_id", descending: true}]}};
   axios.post(rpcAPI, buyOrderQuery).then((resMetrics) => {
-    let buy = []
+    let buy = [];
     for (i in resMetrics.data.result){
       buy.push(resMetrics.data.result[i].txId)
     }
-    let sellOrderQuery = {id: 0,jsonrpc: "2.0",method: "find",params: {contract: "market", table: "sellBook", query: {account : config.username, symbol : config.symbol}, limit: 1000, offset: 0, indexes: [{index: "_id", descending: true}]}}
+    let sellOrderQuery = {id: 0,jsonrpc: "2.0",method: "find",params: {contract: "market", table: "sellBook", query: {account : config.username, symbol : config.symbol}, limit: 1000, offset: 0, indexes: [{index: "_id", descending: true}]}};
     axios.post(rpcAPI, sellOrderQuery).then((resMetrics) => {
-      let sell = []
+      let sell = [];
       for (i in resMetrics.data.result){
         sell.push(resMetrics.data.result[i].txId)
       }
-      let c = 1
-      let max = buy.length + sell.length
+      let c = 1;
+      let max = buy.length + sell.length;
       for (i in buy){
-        cancelOrder("buy", buy[i], c, max)
+        cancelOrder("buy", buy[i], c, max);
         c++
       }
       for (i in sell){
-        cancelOrder("sell", sell[i], c, max)
+        cancelOrder("sell", sell[i], c, max);
         c++
       }
-      if (max == 0){
+      if (max === 0){
         place_orders.placeOrders()
       }
     })
@@ -37,15 +37,15 @@ function cancelOrders(){
 
 function cancelOrder(type, id, count, max){
   setTimeout(() => {
-    let cancelJson = {"contractName":"market","contractAction":"cancel","contractPayload":{"type":`${type}`,"id":`${id}`}}
-    console.log(`Attempting to cancel ${type} order with id ${id}.`)
-    hive.broadcast.customJson(config.privateActiveKey, [config.username], null, "ssc-mainnet-hive", JSON.stringify(cancelJson), (err, result) => {
+    let cancelJson = {"contractName":"market","contractAction":"cancel","contractPayload":{"type":`${type}`,"id":`${id}`}};
+    console.log(`Attempting to cancel ${type} order with id ${id}.`);
+    hive.broadcast.customJson(config.privateActiveKey, [config.username], null, "ssc-mainnet-hive", JSON.stringify(cancelJson), (err) => {
       if (err){
         console.log(`Error canceling order with id ${id}.`)
       } else {
         console.log(`Successfully canceled order with id ${id}.`)
       }
-      if (count == max){
+      if (count === max){
         setTimeout(() => {
           place_orders.placeOrders()
         }, 10 * 1000)
@@ -56,4 +56,4 @@ function cancelOrder(type, id, count, max){
 
 module.exports = {
   cancelOrders
-}
+};
